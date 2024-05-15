83a71996ffd79a9ed091b5e9e0fba5509045ebbb
Melanie <caafpiraat@gmail.com>
2024-05-15 12:02:00 +0200

    feat: Add robot velocity

--- src/main/java/components/Velocity.java
+++ src/main/java/components/Velocity.java
@@ -1,8 +1,9 @@
 package components;
 
 public class Velocity {
-    private double[] xyradiansVelocity;
+    private double[] robotVelocity;
     private double[] ballVelocity;
+    private int RADIANS_INDEX = 2;
 
     public void setBallVelocity(double[] ballPosition, double[] vector){
         ballVelocity = originTranslation(ballPosition, vector);
@@ -12,6 +13,31 @@ public class Velocity {
         return ballVelocity;
     }
 
+    public void setRobotVelocity(double[] robotPosition, double[] vector){
+        robotVelocity = originTranslationRadians(robotPosition, vector);
+    }
+
+    public double[] getRobotVelocity(){
+        return robotVelocity;
+    }
+
+
+    private double[] originTranslationRadians(double[] robot, double[] vector){
+        if (robot.length != vector.length) {
+            throw new IllegalArgumentException("Arrays must be of equal length");
+        }
+        double[] result = new double[robot.length];
+        for (int i = 0; i < RADIANS_INDEX; i++){
+            result[i] = robot[i] + vector[i];
+        }
+        
+        double angle = robot[RADIANS_INDEX] + vector[RADIANS_INDEX];
+        angle = (angle + 2 * Math.PI) % (2 * Math.PI); 
+
+        result[RADIANS_INDEX] = angle;
+
+        return result;
+    }
 
     private double[] originTranslation(double[]object, double[] vector){
         if (object.length != vector.length) {
@@ -23,5 +49,6 @@ public class Velocity {
             result[i] = object[i] + vector[i];
         }
         return result;
-    }
+    }   
+    
 }
--- src/test/java/components/VelocityTest.java
+++ src/test/java/components/VelocityTest.java
@@ -36,4 +36,27 @@ public class VelocityTest {
          assertThrows(IllegalArgumentException.class, () -> {
             velocity.setBallVelocity(invalidBallPosition, velocityVector);});
     }
+
+    @Test
+    public void itDoesCorrectOriginTranslationForRobots(){
+        double[] robotPosition = {3, -2, 0.5};
+        double[] velocityVector = {-0.000472, -0.000724, 0.001935};
+
+        double[] expectedVector = {2.999528, -2.000724, 0.501935};
+
+        velocity.setRobotVelocity(robotPosition, velocityVector);
+
+        double[] translatedVector = velocity.getRobotVelocity();
+
+        assertArrayEquals(expectedVector, translatedVector, 0.0001);
+    }
+
+    @Test
+    public void itThrowsAnExceptionWhenPositionAndVectorsDoNotMatch(){
+        double[] invalidRobotPosition = {3, -2, 0.5, 10};
+        double[] velocityVector = {-0.000472, -0.000724, 0.001935};
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            velocity.setBallVelocity(invalidRobotPosition, velocityVector);});
+    }
 }
