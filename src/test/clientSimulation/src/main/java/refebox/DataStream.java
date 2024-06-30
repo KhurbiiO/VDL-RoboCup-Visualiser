d06a6565f5729e583e26316d9c9df800a0b05379
Hô,Kiet T.K <k.ho@student.fontys.nl>
2024-06-30 13:57:01 +0200

    update client multicast simulation and udp clietn

---
+++ src/test/clientSimulation/src/main/java/refebox/DataStream.java
@@ -0,0 +1,63 @@
+package refebox;
+
+import java.io.IOException;
+import java.net.*;
+import org.json.JSONObject;
+
+public class DataStream extends Thread {
+    /////////////////// fields ///////////////////////
+    private DataObserver observer;
+    private MulticastSocket ms;
+
+    // multicast destination
+    InetAddress multiIp;
+    int multiPort;
+
+    ////////////////// constructor ////////////////
+    public DataStream(String dataSource, InetAddress multiIp, int multiPort) throws IOException {
+        this.observer = new DataObserver(dataSource);
+        this.ms = new MulticastSocket();
+
+        this.multiIp = multiIp;
+        this.multiPort = multiPort;
+    }
+
+    ///////////////// methods ////////////////////
+    public void SendData(JSONObject data) throws IOException {
+        // serialising the json object into bytes
+        String s_data = data.toString();
+        byte buff[] = s_data.getBytes();
+       
+        //debugging - print send data
+        System.out.println(s_data);
+        // packaging and multicast the message
+        DatagramPacket dpSend = new DatagramPacket(buff, buff.length, multiIp, multiPort);
+        ms.send(dpSend);
+       
+
+    }
+
+    // run thread
+    @Override
+    public void run()
+    // stream send data for every 1 second
+    {
+        System.out.println("HELLO FROM DATA STREAM");
+        while (true) {
+            try {
+                JSONObject data = observer.GetData();
+                SendData(data);
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e) {
+
+            }
+            catch(IOException e)
+            {
+
+            }
+        }
+
+    }
+
+}
---
+++ src/test/clientSimulation/src/main/java/refebox/Main.java
@@ -0,0 +1,48 @@
+package refebox;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class Main {
+    public static void main(String[] args)  {
+       try{
+        InetAddress multiAddr1 = InetAddress.getByName("239.255.0.1");
+        InetAddress multiAddr2 = InetAddress.getByName("239.255.0.2");
+
+        int port = 80;
+
+        DataStream dataStream1 = new DataStream("src/main/resources/20180618_085311.A.msl",multiAddr1, 80);
+        DataStream dataStream2 = new DataStream("src/main/resources/20180618_085311.B.msl",multiAddr2, 80);
+
+        dataStream1.start();
+       // dataStream2.start();
+       }
+       catch(Exception e)
+       {
+        System.out.println(e);
+       }
+        // server.start();
+
+        // Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+        // while (networkInterfaces.hasMoreElements()) {
+        //     NetworkInterface networkInterface = networkInterfaces.nextElement();
+
+        //     System.out.println("Name: " + networkInterface.getName());
+        //     System.out.println("Display Name: " + networkInterface.getDisplayName());
+        //     System.out.println("Is up? " + networkInterface.isUp());
+        //     System.out.println("Is loopback? " + networkInterface.isLoopback());
+        //     System.out.println("Is virtual? " + networkInterface.isVirtual());
+
+        //     Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+        //     while (inetAddresses.hasMoreElements()) {
+        //         InetAddress inetAddress = inetAddresses.nextElement();
+        //         System.out.println("  InetAddress: " + inetAddress.getHostAddress());
+        //     }
+
+        //     System.out.println("--------------------------");
+        // }
+    }
+}
